b57d4a97a1c07b208b1eabc1b4998f2b1ed79af7
chikutaku <chikutaku.reiwa.2020@gmail.com>
2025-02-24 09:58:20 +0900

    up cdk

--- sample-07-cdk/hello-cdk/lib/hello-cdk-stack.ts
+++ sample-07-cdk/hello-cdk/lib/hello-cdk-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,5 +14,28 @@ export class HelloCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Define the Lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World! これはCDKで作成されたLambda関数です2。updated.'),
+          };
+        };
+      `),
+    });
+
+    // Add the URL output to the stack
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+    // Define a CloudFormation output for your URL
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    })
   }
 }
